9a8b1ed32799ca802d3a1689113825d156ceec53
Abhik09101990 <68180947+Abhik09101990@users.noreply.github.com>
2021-06-20 21:55:10 +0530

    Add files via upload

---
+++ CustomerInformation.aspx
@@ -0,0 +1,148 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CustomerInformation.aspx.cs" Inherits="WebApplication1.CustomerInformation" %>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="js/scripts.js"></script>
+
+
+</head>
+    <script>
+        $(document).ready(function () {
+            alert();
+
+            $("#btnSubmit").click(function (e) {
+                e.preventDefault();
+
+                if ($("#txtFirstName").val() == "") {
+                    alert('First Name mandatory');
+                }
+            });
+
+        });
+
+    </script>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table style="border: 1px solid black; width: 100%; height: 100%; font-size: 20px; font-family: Calibri">
+                <tr>
+                    <td colspan="6" style="text-align: center; font-weight: bold; font-size: 50px; width: 100%; padding-bottom: 50px">Customer Information Form</td>
+                </tr>
+                <tr>
+                    <td style="width: 20%">Name of Customer</td>
+                    <td style="width: 20%">
+                        <table style="width: 100%;" cellpadding="0" cellspacing="0">
+                            <tr>
+                                <td style="padding-right: 5px">
+                                    <asp:TextBox ID="txtFirstName" runat="server" MaxLength="20"></asp:TextBox>
+                                </td>
+                                <td>
+                                    <asp:TextBox ID="txtLastName" runat="server" MaxLength="10"></asp:TextBox>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                    <td style="width: 10%">PAN No.</td>
+                    <td style="width: 20%">
+                        <asp:TextBox ID="txtPanNo" runat="server" MaxLength="10"></asp:TextBox>
+                    </td>
+                    <td style="width: 20%">Address</td>
+                    <td style="width: 10%">
+                        <asp:TextBox ID="txtAddress" runat="server" MaxLength="50"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Pin Code</td>
+                    <td>
+                        <asp:TextBox ID="txtPinCode" runat="server" TextMode="Number" MaxLength="6"></asp:TextBox></td>
+                    <td>City</td>
+                    <td>
+                        <asp:TextBox ID="txtCity" runat="server" MaxLength="30"></asp:TextBox>
+                    </td>
+                    <td>State</td>
+                    <td>
+                        <asp:TextBox ID="txtState" runat="server" MaxLength="20"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Paid Fee</td>
+                    <td>
+                        <asp:RadioButtonList ID="rbFeeConfirm" runat="server" RepeatDirection="Horizontal">
+                            <asp:ListItem Text="Yes" Value="1" Selected="True"></asp:ListItem>
+                            <asp:ListItem Text="No" Value="0"></asp:ListItem>
+                        </asp:RadioButtonList></td>
+                    <td>Amount</td>
+                    <td>
+                        <asp:TextBox ID="nbxAmount" runat="server" TextMode="Number" MaxLength="10"></asp:TextBox></td>
+                    <td>Date of Payment</td>
+                    <td>
+                        <asp:TextBox ID="dtbPayment" runat="server" TextMode="Date"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td style="text-align: left;">
+                        <asp:TextBox ID="txtSrlNo" runat="server" TextMode="Number" Enabled="false" Visible="false" ReadOnly="true"></asp:TextBox>
+                    </td>
+                    <td colspan="3"></td>
+                </tr>
+                <tr>
+                    <td colspan="3" style="text-align: right; padding-top: 30px">
+                        <asp:Button ID="btnAdd" OnClick="btnAdd_Click" runat="server" Text="Add" Style="font-weight: bold; font-size: medium; color: blue; border: solid" />
+                        <asp:Button ID="btnEdit" OnClick="btnEdit_Click" runat="server" Text="Update" Style="font-weight: bold; font-size: medium; color: orange; border: solid" />
+                        <asp:Button ID="btnDelete" OnClick="btnDelete_Click" runat="server" Text="Delete" Style="font-weight: bold; font-size: medium; color: red; border: solid" />
+                    </td>
+                    <td colspan="3" style="text-align: left; padding-top: 30px">
+                        <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" Style="font-weight: bold; font-size: medium; color: green; border: solid" />
+                        <%--<asp:Button ID="btnCancel" runat="server" OnClick="btnCancel_Click" Text="Cancel" Style="font-weight: bold; font-size: medium; color: red; border: solid" />--%>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="6">
+                        <asp:GridView ID="grdCustomer" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" Width="100%" OnSelectedIndexChanged="grdCustomer_SelectedIndexChanged" AutoGenerateColumns="false" OnRowDataBound="grdCustomer_RowDataBound">
+                            <AlternatingRowStyle BackColor="White" />
+                            <Columns>
+                                <asp:CommandField ShowSelectButton="True" />
+                                <asp:BoundField DataField="SrlNo" HeaderText="SrlNo" HeaderStyle-HorizontalAlign="Left" ItemStyle-HorizontalAlign="Left" />
+                                <asp:TemplateField HeaderText="Customer Name" HeaderStyle-HorizontalAlign="Left" ItemStyle-HorizontalAlign="Left">
+                                    <ItemTemplate>
+                                        <%# string.Format("{0} {1}", Eval("FirstName") ,Eval("LastName"))%>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:BoundField DataField="PanNo" HeaderText="Pan No" HeaderStyle-HorizontalAlign="Left" ItemStyle-HorizontalAlign="Left" />
+                                <asp:BoundField DataField="Address" HeaderText="Address" HeaderStyle-HorizontalAlign="Left" ItemStyle-HorizontalAlign="Left" />
+                                <asp:BoundField DataField="PinCode" HeaderText="Pin Code" HeaderStyle-HorizontalAlign="Left" ItemStyle-HorizontalAlign="Left" />
+                                <asp:BoundField DataField="City" HeaderText="City" HeaderStyle-HorizontalAlign="Left" ItemStyle-HorizontalAlign="Left" />
+                                <asp:BoundField DataField="State" HeaderText="State" HeaderStyle-HorizontalAlign="Left" ItemStyle-HorizontalAlign="Left" />
+                                <asp:TemplateField HeaderText="Paid Fee" InsertVisible="false" HeaderStyle-HorizontalAlign="Center" ItemStyle-HorizontalAlign="Center">
+                                    <ItemTemplate>
+                                        <asp:CheckBox ID="CheckBox1" runat="server" Visible="true" Checked='<%# Eval("PaidFee") %>' Enabled="false" />
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:BoundField DataField="Amount" HeaderText="Amount" HeaderStyle-HorizontalAlign="Right" ItemStyle-HorizontalAlign="Right" />
+                                <asp:BoundField DataField="DateofPayment" HeaderText="Date of Payment" DataFormatString="{0:MM/dd/yyyy}" HeaderStyle-HorizontalAlign="Left" ItemStyle-HorizontalAlign="Left" />
+                            </Columns>
+                            <EditRowStyle BackColor="#7C6F57" />
+                            <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                            <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                            <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+                            <RowStyle BackColor="#E3EAEB" />
+                            <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+                            <SortedAscendingCellStyle BackColor="#F8FAFA" />
+                            <SortedAscendingHeaderStyle BackColor="#246B61" />
+                            <SortedDescendingCellStyle BackColor="#D4DFE1" />
+                            <SortedDescendingHeaderStyle BackColor="#15524A" />
+                        </asp:GridView>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="6">
+                        <asp:Label ID="lblMessage" runat="server"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CustomerInformation.aspx.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class CustomerInformation : System.Web.UI.Page
+    {
+        private string connstr = string.Empty;
+        private string buttoncommand = string.Empty;
+        private long   msrlno = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            connstr = ConfigurationManager.ConnectionStrings["custcondb"].ConnectionString;
+
+            if (!IsPostBack)
+            {
+                setControlState(false);
+                LoadFirstCustomerData();
+                LoadCustomerGrid();
+            }
+        }
+
+        private bool CheckCustomerData()
+        {
+            string lSQL = "select count(*) from customer where [FirstName] = '" + txtFirstName.Text.Replace("'", "''") + "' and [LastName] = '" + txtLastName.Text.Replace("'", "''") + "';";
+
+            SqlCommand cmd = null;
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connstr))
+                {
+                    cmd = new SqlCommand(lSQL, con);
+                    cmd.CommandType = CommandType.Text;
+
+                    con.Open();
+                    int lcustcount = Convert.ToInt32(cmd.ExecuteScalar());
+
+                    if (lcustcount > 0)
+                    {
+                        lblMessage.Text = "Duplicate customer information exists.";
+                        return false;
+                    }
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = "Error in Connection " + "[" + ex.Message + "]";
+                return false;
+            }
+            finally
+            {
+                cmd.Dispose();
+            }
+        }
+
+        private void setControlState(bool vEnable)
+        {
+            if (!string.IsNullOrEmpty(buttoncommand))
+                if (buttoncommand == "Add")
+                {
+                    txtFirstName.Enabled = true;
+                    txtLastName.Enabled = true;
+                }
+                else
+                {
+                    txtFirstName.Enabled = false;
+                    txtLastName.Enabled = false;
+                }
+            else
+            {
+                txtFirstName.Enabled = vEnable;
+                txtLastName.Enabled = vEnable;
+            }
+            // txtCustomer.Enabled = (buttoncommand == "Add" || buttoncommand == "Update") ? !vEnable : vEnable;
+            txtPanNo.Enabled = vEnable;
+            txtAddress.Enabled = vEnable;
+            txtPinCode.Enabled = vEnable;
+            txtCity.Enabled = vEnable;
+            txtState.Enabled = vEnable;
+            rbFeeConfirm.Enabled = vEnable;
+            nbxAmount.Enabled = vEnable;
+            dtbPayment.Enabled = vEnable;
+            btnAdd.Enabled = !vEnable;
+            btnEdit.Enabled = !vEnable;
+            btnDelete.Enabled = !vEnable;
+            btnSubmit.Enabled = vEnable;
+
+            if (!btnSubmit.Enabled)
+                btnSubmit.Visible = false;
+            else
+                btnSubmit.Visible = true;
+        }
+
+        private long GetMaxSrlNo()
+        {
+            string lSQL = "select isnull(max([SrlNo]),0) + 1 from customer;";
+
+            SqlCommand cmd = null;
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connstr))
+                {
+                    cmd = new SqlCommand(lSQL, con);
+                    cmd.CommandType = CommandType.Text;
+
+                    con.Open();
+                    return Convert.ToInt64(cmd.ExecuteScalar());
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = "Error in Connection " + "[" + ex.Message + "]";
+                return 0;
+            }
+            finally
+            {
+                cmd.Dispose();
+            }
+        }
+
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (!CheckCustomerData())
+                return;
+
+            SqlCommand cmd = null;
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connstr))
+                {
+                    if (ViewState["OP"].ToString() == "Add")
+                        msrlno = GetMaxSrlNo();
+                    else
+                        msrlno = Convert.ToInt64(txtSrlNo.Text);
+                    string custno = txtFirstName.Text.Substring(0,1) + txtPanNo.Text;
+
+                    cmd = new SqlCommand("sp_customers", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@operation", ViewState["OP"].ToString());
+                    cmd.Parameters.AddWithValue("@srlno", msrlno);
+                    cmd.Parameters.AddWithValue("@firstname", txtFirstName.Text.ToUpper().Replace("'", "''"));
+                    cmd.Parameters.AddWithValue("@lastname", txtLastName.Text.ToUpper().Replace("'", "''"));
+                    cmd.Parameters.AddWithValue("@panno", txtPanNo.Text.ToUpper().Replace("'", "''"));
+                    cmd.Parameters.AddWithValue("@address", txtAddress.Text.ToUpper().Replace("'", "''"));
+                    cmd.Parameters.AddWithValue("@pincode", txtPinCode.Text.ToUpper().Replace("'", "''"));
+                    cmd.Parameters.AddWithValue("@city", txtCity.Text.ToUpper().Replace("'", "''"));
+                    cmd.Parameters.AddWithValue("@state", txtState.Text.ToUpper().Replace("'", "''"));
+                    cmd.Parameters.AddWithValue("@paidfee", rbFeeConfirm.SelectedValue);
+                    cmd.Parameters.AddWithValue("@amount", nbxAmount.Text.Replace("'", "''"));
+                    cmd.Parameters.AddWithValue("@dateofpayment", dtbPayment.Text.Replace("'", "''"));
+                    cmd.Parameters.AddWithValue("@custno", custno);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+
+                    LoadCustomerGrid();
+
+                    lblMessage.Text = "Customer Information has been added successfully and Customer Id is "+custno;
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = "Error in Connection " + "[" + ex.Message + "]";
+            }
+            finally
+            {
+                cmd.Dispose();
+            }
+            setControlState(false);
+        }
+
+        private DataSet LoadCustomerGrid()
+        {
+            string lSQL = string.Empty;
+            DataSet lDS = new DataSet();
+            SqlDataAdapter da = null;
+
+            lSQL = "Select * from customer;";
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connstr))
+                {
+                    con.Open();
+                    da = new SqlDataAdapter(lSQL, con);
+                    da.Fill(lDS);
+                    grdCustomer.DataSource = lDS;
+                    grdCustomer.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = "Error in Connection " + "[" + ex.Message + "]";
+            }
+            finally
+            {
+                da.Dispose();
+            }
+            return lDS;
+        }
+
+        private void LoadFirstCustomerData()
+        {
+            string lSQL = string.Empty;
+            SqlDataReader rdr = null;
+            SqlCommand cmd = null;
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connstr))
+                {
+                    con.Open();
+                    cmd = new SqlCommand("select top 1 * from customer;", con);
+
+                    rdr = cmd.ExecuteReader();
+
+                    if (rdr.HasRows)
+                    {
+                        while (rdr.Read())
+                        {
+                            txtFirstName.Text = (string)rdr["FirstName"];
+                            txtLastName.Text = (string)rdr["LastName"];
+                            txtPanNo.Text = (string)rdr["PanNo"];
+                            txtAddress.Text = (string)rdr["Address"];
+                            txtPinCode.Text = rdr["PinCode"].ToString();
+                            txtCity.Text = (string)rdr["City"];
+                            txtState.Text = (string)rdr["State"];
+                            if (Convert.ToBoolean(rdr["PaidFee"]).ToString().ToUpper() == "TRUE")
+                                rbFeeConfirm.SelectedValue = "1";
+                            else
+                                rbFeeConfirm.SelectedValue = "0";
+                            nbxAmount.Text = Convert.ToDouble(rdr["Amount"]).ToString();
+                            dtbPayment.Text = Convert.ToDateTime(rdr["DateofPayment"]).ToString("yyyy-MM-dd");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = "Error in Connection " + "[" + ex.Message + "]";
+            }
+            finally
+            {
+                rdr.Close();
+                cmd.Dispose();
+            }
+        }
+
+        private void GetData(long vsrlno)
+        {
+            string lSQL = string.Empty;
+            SqlDataReader rdr = null;
+            SqlCommand cmd = null;
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connstr))
+                {
+                    con.Open();
+                    cmd = new SqlCommand("select * from customer where [SrlNo] = " + vsrlno + ";", con);
+
+                    rdr = cmd.ExecuteReader();
+
+                    if (rdr.HasRows)
+                    {
+                        while (rdr.Read())
+                        {
+                            txtSrlNo.Text = vsrlno.ToString();
+                            txtFirstName.Text = (string)rdr["FirstName"];
+                            txtLastName.Text = (string)rdr["LastName"];
+                            txtPanNo.Text = (string)rdr["PanNo"];
+                            txtAddress.Text = (string)rdr["Address"];
+                            txtPinCode.Text = rdr["PinCode"].ToString();
+                            txtCity.Text = (string)rdr["City"];
+                            txtState.Text = (string)rdr["State"];
+                            if (Convert.ToBoolean(rdr["PaidFee"]).ToString().ToUpper() == "TRUE")
+                                rbFeeConfirm.SelectedValue = "1";
+                            else
+                                rbFeeConfirm.SelectedValue = "0";
+                            nbxAmount.Text = Convert.ToDouble(rdr["Amount"]).ToString();
+                            dtbPayment.Text = Convert.ToDateTime(rdr["DateofPayment"]).ToString("yyyy-MM-dd");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = "Error in Connection " + "[" + ex.Message + "]";
+            }
+            finally
+            {
+                rdr.Close();
+                cmd.Dispose();
+            }
+        }
+
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            buttoncommand = (string)(sender as Button).Text;
+            ViewState["OP"] = buttoncommand;
+            setControlState(true);
+        }
+
+        public void func_clearform()
+        {
+
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtPanNo.Text = "";
+            txtAddress.Text = "";
+            txtPinCode.Text = "";
+            txtCity.Text = "";
+            txtState.Text = "";
+            txtSrlNo.Text = "0";
+
+        }
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            func_clearform();
+            buttoncommand = (string)(sender as Button).Text;
+            ViewState["OP"] = buttoncommand;
+            setControlState(true);
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            buttoncommand = (string)(sender as Button).Text;
+            ViewState["OP"] = buttoncommand;
+            setControlState(false);
+        }
+
+        protected void grdCustomer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            long lsrlno = Convert.ToInt64(grdCustomer.SelectedRow.Cells[1].Text);
+            GetData(lsrlno);
+        }
+
+        protected void grdCustomer_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if(e.Row.RowType == DataControlRowType.DataRow || e.Row.RowType == DataControlRowType.Header)
+            {
+                e.Row.Cells[1].Visible = false;
+            }
+        }
+    }
+}
